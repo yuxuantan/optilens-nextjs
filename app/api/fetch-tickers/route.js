f31f89e7c79cb48aeb7d0be72b147ba32a5ed62f
YX <yu_xuan_tan@hotmail.com>
2024-10-04 14:24:51 +0800

    remove experimental ppr

--- app/api/fetch-tickers/route.js
+++ app/api/fetch-tickers/route.js
@@ -1,6 +1,4 @@
 // app/api/fetch-tickers/route.js
-export const experimental_ppr = true
-
 export async function GET() {
     try {
       const response = await fetch('https://www.sec.gov/files/company_tickers.json');
